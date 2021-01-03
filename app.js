f18120cda2ef09900efa9d5ace175f47aad6bf2c
Facundojs <facuserra2002@gmailcom>
2021-01-03 16:53:46 -0300

    Animaciones

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.listen(3000, () => {
     
 });
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/views/home.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html')))
+
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html')))
+
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/views/login.html')))
